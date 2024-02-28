6829cf1ba09db48077039a26a37b5f1227daa769
88um <atriciaurray@gmail.com>
2024-02-28 15:41:31 -0600

    More user friedly

--- src/main.py
+++ src/main.py
@@ -1,18 +1,24 @@
 import os
 from reel_poster import ReelReposter
 from dotenv import load_dotenv
-load_dotenv()
 
-session_id = os.getenv('TEST_SESSION')
-client = ReelReposter(config_file='config.json', media_folder='media')
-client.login(session_id)
-#client.login('username', 'password')
-#Login either by session or credentials
+if __name__ == "__main__":
+    load_dotenv()
+    session_id = os.getenv('TEST_SESSION')
+    client = ReelReposter(config_file='config.json', media_folder='media')
+    loggedin = False
 
-
-def main():
-
-    while True:
+    if session_id:
+        loggedin = client.login(session_id)
+    
+    
+    else:
+        username = input("[LOGIN] Type your username: ")
+        password = input("[LOGIN] Type your password: ")
+        loggedin = client.login(username, password)
+    #Login either by session or credentials
+    
+    while loggedin:
         if not client.has_more_posts():
             # Collect posts and save them to folder
             posts = client.scrape_new_posts()
@@ -23,7 +29,7 @@ def main():
 
         #Now upload it!
         uploaded = client.upload_saved_post(post)
-        print("Successfully uploaded post!") if uploaded else print("Failed to upload post!")
+        print(f"[{client.client.username}] Successfully uploaded post! | Total Done: {client.done}") if uploaded else print("Failed to upload post!")
 
         #Now check if any of my recent posts are going viral, if so - comment and tell people to follow me
         client.check_viral()
